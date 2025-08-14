779a15099e03838909d40c2d843453ae0884db62
akazaa1 <akazaa1122@gmail.com>
2025-08-14 20:51:23 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,13 +8,6 @@ api = Api(app)
 class Greeting (Resource):
     def get(self):
         return "mortada is Up & Running!"
-print("  **     **  ")
-print(" ****   **** ")
-print("*************")
-print(" *********** ")
-print("   *******   ")
-print("     ***     ")
-print("      *      ")
 
 api.add_resource(Greeting, '/')
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
